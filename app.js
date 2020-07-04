ef0a1468e2e62ef3ca410c63ce4b23d55c0b1844
Shikhar Subedi <shikharsubedi@Shikhars-MacBook-Pro.local>
2020-07-04 16:09:42 -0700

    updated with mongodb username and password as environment variables

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+'use strict'
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
